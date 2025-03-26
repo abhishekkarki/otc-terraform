72ebe81c0c7567f66f5d5cc5047ded1876c2d707
abhishekkarki <47475044+abhishekkarki@users.noreply.github.com>
2025-03-26 16:07:14 +0100

    Update vpc.tf

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -1,27 +1,33 @@
-resource "openstack_networking_network_v2" "network" {
-  name           = "${var.name}-network"
-  admin_state_up = "true"
+
+resource "opentelekomcloud_vpc_v1" "vpc" {
+  description = "Main VPC for the infrastructure"
+  name = var.vpc_name
+  cidr = var.vpc_cidr
 }
 
-resource "openstack_networking_subnet_v2" "subnet" {
-  name            = "${var.name}-subnet"
-  network_id      = "${openstack_networking_network_v2.network.id}"
-  cidr            = "${var.subnet}"
-  ip_version      = 4
-  dns_nameservers = ["${var.nameserver}"]
+resource "opentelekomcloud_vpc_subnet_v1" "subnet1" {
+  name   = var.subnet1_name
+  vpc_id = opentelekomcloud_vpc_v1.vpc.id
+  cidr   = var.subnet1_cidr
+  #gateway_ip = cidrhost(var.vpc_cidr, 1) # this uses the 1st ip of the vpc 
+  gateway_ip = cidrhost(var.subnet1_cidr, 1) # this uses the 1st ip of the subnet1
+  #az     = var.az
 }
 
-resource "openstack_networking_router_v2" "router" {
-  name                = "${var.name}-router"
-  admin_state_up      = "true"
-  external_network_id = "${data.openstack_networking_network_v2.extnet.id}"
+resource "opentelekomcloud_vpc_subnet_v1" "subnet2" {
+  name   = var.subnet2_name
+  vpc_id = opentelekomcloud_vpc_v1.vpc.id
+  cidr   = var.subnet2_cidr
+  gateway_ip = cidrhost(var.subnet2_cidr, 1)
 }
 
-resource "openstack_networking_router_interface_v2" "interface" {
-  router_id = "${openstack_networking_router_v2.router.id}"
-  subnet_id = "${openstack_networking_subnet_v2.subnet.id}"
+resource "opentelekomcloud_vpc_subnet_v1" "subnet3" {
+  name   = var.subnet3_name
+  vpc_id = opentelekomcloud_vpc_v1.vpc.id
+  cidr   = var.subnet3_cidr
+  gateway_ip = cidrhost(var.subnet3_cidr, 1)
 }
 
-data "openstack_networking_network_v2" "extnet" {
-  name = "${var.ext_net_name}"
-}
+
+
+
