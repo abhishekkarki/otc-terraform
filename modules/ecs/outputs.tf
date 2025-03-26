6826d045d8910d9e8e97ee4e57236dd6d1eefff2
abhishekkarki <47475044+abhishekkarki@users.noreply.github.com>
2025-03-26 16:05:26 +0100

    Update outputs.tf

--- modules/ecs/outputs.tf
+++ modules/ecs/outputs.tf
@@ -1,14 +1,3 @@
-output "addresses" {
-    description = "list of ipv4 addresses of the created servers"
-    value       = ["${openstack_compute_instance_v2.instance.*.access_ip_v4}"]
-}
-
-output "ids" {
-    description = "list of IDs of the created servers"
-    value       = ["${openstack_compute_instance_v2.instance.*.id}"]
-}
-
-output "count" {
-    description = "number of servers created"
-    value       = "${var.ecs_count}"
+output "instance_ips" {
+  value = opentelekomcloud_compute_instance_v2.instances[*].access_ip_v4
 }
