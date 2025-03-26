8f879a62a4d90921ab11aeb6408cb04a2426b4a0
abhishekkarki <47475044+abhishekkarki@users.noreply.github.com>
2025-03-26 16:13:12 +0100

    Update variables.tf

---
+++ variables.tf
@@ -0,0 +1,118 @@
+#------------------------------------------------------------------------------------
+# variables for vault
+#------------------------------------------------------------------------------------
+variable "vault_address" {
+  description = "The address of the Vault server"
+  type        = string
+}
+
+variable "vault_token" {
+  description = "The authentication token for Vault"
+  type        = string
+  sensitive   = true
+}
+#------------------------------------------------------------------------------------
+# variables for VPC 
+#------------------------------------------------------------------------------------
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "subnet1_name" {
+  description = "Name of subnet 1"
+  type        = string
+}
+
+variable "subnet1_cidr" {
+  description = "CIDR block for subnet 1"
+  type        = string
+}
+
+variable "subnet2_name" {
+  description = "Name of subnet 2"
+  type        = string
+}
+
+variable "subnet2_cidr" {
+  description = "CIDR block for subnet 2"
+  type        = string
+}
+
+variable "subnet3_name" {
+  description = "Name of subnet 3"
+  type        = string
+}
+
+variable "subnet3_cidr" {
+  description = "CIDR block for subnet 3"
+  type        = string
+}
+
+variable "az" {
+  description = "Availability Zone to deploy the subnets"
+  type        = string
+}
+
+#------------------------------------------------------------------------------------
+# variables for security group
+#------------------------------------------------------------------------------------
+variable "security_group1_name" {
+  description = "Name of the security group"
+  type        = string
+}
+
+variable "security_group2_name" {
+  description = "Name of the security group"
+  type        = string
+}
+
+variable "security_group3_name" {
+  description = "Name of the security group"
+  type        = string
+}
+
+#------------------------------------------------------------------------------------
+# variables for availability zone
+#------------------------------------------------------------------------------------
+variable "availability_zone" {
+  description = "Availability zone for the subnets and VMs"
+  type        = string
+}
+
+#------------------------------------------------------------------------------------
+# variables for VMs
+#------------------------------------------------------------------------------------
+variable "instances" {
+  type = list(object({
+    name                 = string
+    flavor               = string
+    image_id             = string
+    subnet_names         = list(string)
+    security_group_names = list(string)
+    availability_zone    = string
+  }))
+  description = "List of instances with multiple subnet and security group names"
+}
+
+
+variable "ssh_key_name" { type = string }
+
+
+
+
+
+# variable "project_name" {
+#   description = "The project (tenant) abk-test for the OTC account"
+#   type        = string
+# }
+
+
+
+
+
