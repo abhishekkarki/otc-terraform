9ff127672c6e6e6cea38834d05f39a7a24e29cb2
abhishekkarki <47475044+abhishekkarki@users.noreply.github.com>
2025-03-26 16:05:39 +0100

    Update outputs.tf

--- modules/ecs/outputs.tf
+++ modules/ecs/outputs.tf
@@ -1,3 +1,26 @@
-output "instance_ips" {
-  value = opentelekomcloud_compute_instance_v2.instances[*].access_ip_v4
+variable "instances" {
+  type = list(object({
+    name                 = string
+    flavor               = string
+    image_id             = string
+    subnet_names         = list(string) 
+    security_group_names = list(string)
+    availability_zone    = string  
+  }))
+  description = "List of instances with multiple subnet and security group names"
+}
+
+variable "ssh_key_name" {
+  type        = string
+  description = "SSH key name for instances"
+}
+
+variable "subnet_ids" {
+  type        = map(string)
+  description = "Mapping of subnet names to subnet IDs"
+}
+
+variable "security_group_ids" {
+  type        = map(string)
+  description = "Mapping of security group names to security group IDs"
 }
