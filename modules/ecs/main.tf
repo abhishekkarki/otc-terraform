cbdaf429460d5bef6b713032e570444dc31ea438
abhishekkarki <47475044+abhishekkarki@users.noreply.github.com>
2025-03-26 20:56:31 +0100

    Update main.tf

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -1,3 +1,18 @@
+resource "opentelekomcloud_networking_port_v2" "ports" {
+  for_each = var.ports
+
+  name       = each.key
+  network_id = lookup(var.subnet_ids, each.value.subnet_name)
+
+  security_group_ids = [
+    for sg in each.value.security_group_names :
+    lookup(var.security_group_ids, sg)
+  ]
+
+  admin_state_up = true
+}
+#-----------------------------------------------------------------------------------
+
 resource "opentelekomcloud_compute_instance_v2" "instances" {
   count       = length(var.instances)
   name        = var.instances[count.index].name
@@ -6,29 +21,14 @@ resource "opentelekomcloud_compute_instance_v2" "instances" {
   key_pair    = var.ssh_key_name
   availability_zone = var.instances[count.index].availability_zone 
 
-  # Attach primary subnet
-  network {
-    uuid = lookup(var.subnet_ids, var.instances[count.index].subnet_names[0])
-  }
-
-  # Attach additional subnets if available
+  # Attach multiple NICs via ports
   dynamic "network" {
-    for_each = slice(var.instances[count.index].subnet_names, 1, length(var.instances[count.index].subnet_names))
-    content {
-      uuid = lookup(var.subnet_ids, network.value)
+  for_each = toset(var.instances[count.index].port_names)
+  content {
+      port = opentelekomcloud_networking_port_v2.ports[network.value].id
     }
   }
-
-  # Attach multiple ports dynamically
-  dynamic "network" {
-    for_each = toset(var.instances[count.index].port_names)
-    content {
-      port = opentelekomcloud_networking_port_v2.ports["${var.instances[count.index].name}-${network.value}"].id
-    }
-  }
-
-  # Attach multiple security groups dynamically
-  security_groups = [for sg in var.instances[count.index].security_group_names : lookup(var.security_group_ids, sg)]
 }
+#-----------------------------------------------------------------------------------
 
 
